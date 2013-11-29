cd55ddc042f4413cd20349189a9fe2c1e3b2e7ed
山形　友佳子 <saji.yukako@ark-info-sys.co.jp>
2013-11-29 11:29:24 +0900

    v1.2.0 バージョンアップ 主な新機能や改善内容 ・画面デザインを一新し、フラットデザインを採用 ・推奨ブラウザをInternet ExploreからGoogle Chromeに変更 ・JasperReportsのバージョンをJasperreport-5.2.0、Jasperreport-fonts- 5.0.4に変更 ・税端数処理の操作性の改善 ・商品や顧客などのポップアップ検索の操作性の改善

--- DB/update/1.1/02_INSERT_MST.sql
+++
@@ -1,9 +0,0 @@
--- メニューの調整
-INSERT INTO MENU_MST_SALES VALUES ('1307','倉庫',NULL,'/master/searchWarehouse','0013','7','1','FFFFFF','339966','初期登録',now(),'ARK','初期登録',now(),'ARK',NULL,NULL,NULL);
-INSERT INTO ROLE_CFG_SALES VALUES ('088','1307','1','初期登録',now(),'ARK','初期登録',now(),'ARK',NULL,NULL,NULL);
-INSERT INTO ROLE_CFG_SALES VALUES ('089','1307','2','初期登録',now(),'ARK','初期登録',now(),'ARK',NULL,NULL,NULL);
-UPDATE ROLE_MST_SALES SET NAME = '棚番（参照）' WHERE ROLE_ID = '017';
-INSERT INTO ROLE_MST_SALES VALUES ('089','倉庫',NULL,'初期登録',now(),'ARK','初期登録',now(),'ARK',NULL,NULL,NULL);
-INSERT INTO ROLE_MST_SALES VALUES ('088','倉庫（参照）',NULL,'初期登録',now(),'ARK','初期登録',now(),'ARK',NULL,NULL,NULL);
-
-
